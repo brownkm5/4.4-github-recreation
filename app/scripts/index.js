52ac09d88ef5060573e13d0bb2045a2ce925a969
Kevin Brown <brownkm5@g.cofc.edu>
2016-10-06 15:04:36 -0400

    first commit

---
+++ app/scripts/index.js
@@ -0,0 +1,13 @@
+
+var $ = require('jquery');
+var _ = require('underscore');
+var Handlebars = require('handlebars');
+var githubtoken = require('./gitapikey.js');
+
+if(githubtoken !== undefined){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + githubtoken.token
+    }
+  });
+}
