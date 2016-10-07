fd8eb5fd41aa058a3a0318724a2e91dd0067cd48
Kevin Brown <brownkm5@g.cofc.edu>
2016-10-07 14:29:51 -0400

    content shown on page

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -11,3 +11,31 @@ if(githubtoken !== undefined){
     }
   });
 }
+$.ajax('https://api.github.com/users/brownkm5').then(displayUserInfo);
+
+$.ajax('https://api.github.com/users/brownkm5/repos').then(displayUserRepo);
+
+// $.ajax('https://api.github.com/users/brownkm5/orgs').then(displayUserCategories);
+
+function displayUserInfo(data){
+  console.log(data);
+  var user = data;
+  var $profileContainer = $('aside');
+
+  var source = $('#aside-template').html();
+  var template = Handlebars.compile(source);
+
+  $profileContainer.append(template(user));
+}
+
+function displayUserRepo(data){
+  console.log(data);
+  var repos = data;
+  var $repoContainer = $('#repo-list'); //ul that holds the list of repos
+
+  var source = $('#repo-template').html();
+  var template = Handlebars.compile(source);
+  _.each(repos, function(repo){
+    $repoContainer.append(template(repo));
+  });
+}
